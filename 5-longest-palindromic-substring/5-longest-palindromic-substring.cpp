6b50c12570554b1bd779f25e61083388641cdf5f
Kartike Tiwari <84660976+23kartik@users.noreply.github.com>
2022-02-08 23:58:28 +0530

    Time: 245 ms (42.24%), Space: 8.4 MB (64.52%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     string longestPalindrome(string str) {
-        int n = str.size();
-
+    int n = str.size();
     bool table[n][n];
  
     memset(table, 0, sizeof(table));
  
     int maxLength = 1;
  
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < n; i++)
         table[i][i] = true;
  
     int start = 0;
-    for (int i = 0; i < n - 1; ++i) {
+    for (int i = 0; i < n - 1; i++) {
         if (str[i] == str[i + 1]) {
             table[i][i + 1] = true;
             start = i;
@@ -22,8 +21,8 @@ public:
     }
  
     
-    for (int k = 3; k <= n; ++k) {
-        for (int i = 0; i < n - k + 1; ++i) {
+    for (int k = 3; k <= n; k++) {
+        for (int i = 0; i < n - k + 1; i++) {
             int j = i + k - 1;
  
             if (table[i + 1][j - 1] && str[i] == str[j]) {
@@ -38,7 +37,7 @@ public:
     }
  
 string s="";
-         for (int i = start; i <= start + maxLength - 1; ++i){
+         for (int i = start; i <= start + maxLength - 1; i++){
              s+=str[i];
          }
        
