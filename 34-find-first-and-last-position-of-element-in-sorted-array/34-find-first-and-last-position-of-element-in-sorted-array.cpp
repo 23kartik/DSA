fddffcb76d70642416082c506600ce8e7a72dcf8
Kartike Tiwari <84660976+23kartik@users.noreply.github.com>
2022-03-05 02:01:42 +0530

    Time: 11 ms (58.49%), Space: 13.7 MB (65.37%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,47 +1,47 @@
 class Solution {
 public:
   
-vector<int> searchRange(vector<int>& a, int target) {
-	int s = 0, e = a.size()-1;
+    vector<int> searchRange(vector<int>& nums, int target) {
+       int low = 0, high = nums.size()-1;
 	vector<int> ans(2, -1);
-	//first occurrence
-	while(s <= e){
-		int m = s + (e-s)/2;
-		if(a[m] < target)
-			s = m+1;
-		else if(a[m] > target)
-			e = m-1;
+
+	while(low <= high){
+		int mid = (low+high)>>1;
+		if(nums[mid] < target)
+			low = mid+1;
+		else if(nums[mid] > target)
+			high = mid-1;
 		else{
-			if(m == s || a[m] != a[m-1]){
-				ans[0] = m;
+			if(mid == low || nums[mid] != nums[mid-1]){
+				ans[0] = mid;
 				break;
 			}
 			else{
-				e = m-1;
-				ans[0] = m-1;
+				high = mid-1;
+				ans[0] = mid-1;
 			}
 		}
 	}
 
-	//last occurrence
-	s = 0, e = a.size()-1;
-	while(s <= e){
-		int m = s + (e-s)/2;
-		if(a[m] < target)
-			s = m+1;
-		else if(a[m] > target)
-			e = m-1;
+
+	 low = 0, high = nums.size()-1;
+	while(low <= high){	
+		int mid = (low+high)>>1;
+		if(nums[mid] < target)
+			low = mid+1;
+		else if(nums[mid] > target)
+			high = mid-1;
 		else{
-			if(m == e || a[m] != a[m+1]){
-				ans[1] = m;
+			if(mid == high || nums[mid] != nums[mid+1]){
+				ans[1] = mid;
 				break;
 			}
 			else{
-				s = m+1;
-				ans[1] = m+1;
+				low = mid+1;
+				ans[1] = mid+1;
 			}
 		}
 	}
 	return ans; 
-}
+    }
 };
