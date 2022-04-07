41538d987d4d20f58b067b8eb2048bfa94ab1a36
Kartike Tiwari <84660976+23kartik@users.noreply.github.com>
2022-04-08 02:00:04 +0530

    Time: 189 ms (19.71%), Space: 14.3 MB (72.57%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -4,15 +4,14 @@ public:
         sort(coins.begin(),coins.end());
         int i,j,n=coins.size();
        vector<int>dp(amount+1,INT_MAX);
-        // for(i=0;i<amount+1;i++){
-        //     dp[i]=INT_MAX;
-        // }
+
         dp[0]=0;
         for(i=1;i<amount+1;i++){
-            for(auto c:coins){
-                if(i-c<0)break;
-               if(dp[i-c]!=INT_MAX){
-                    dp[i]=min(dp[i],dp[i-c]+1);
+            for(j=0;j<n;j++){
+                if(i-coins[j]<0)break;
+               
+                if(dp[i-coins[j]]!=INT_MAX){
+                    dp[i]=min(dp[i],dp[i-coins[j]]+1);
                 }
             }
         }
