efe8d0df1130fb62db3f41773aabc6a80ee54023
Kartike Tiwari <84660976+23kartik@users.noreply.github.com>
2022-04-17 11:43:33 +0530

    Time: 73 ms (91.74%), Space: 27.9 MB (96.33%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -10,20 +10,6 @@ public:
             adjmat[roads[i][1]][roads[i][0]]=1;
         }
         int ans=0;
-//         int maxi=-1,maxi2=-1,indexmaxi=-1,indexmaxi2=-1,ans=0;
-  
-//         for(int i=0;i<n;i++){
-//         if(degree[i]>=maxi){
-//             maxi2 = maxi;
-//             maxi = degree[i];
-//             indexmaxi2 = indexmaxi;
-//             indexmaxi = i;
-//         }
-//             else if (degree[i] >= maxi2) { 
-//             maxi2 = degree[i]; 
-//             indexmaxi2 = i;
-//         }
-//         }
        for (int i = 0; i < n; i++) {
 	    for (int j = i + 1; j < n; j++) {
 		ans = max(ans, degree[i] + degree[j] - adjmat[i][j] );  
