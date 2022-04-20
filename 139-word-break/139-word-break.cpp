8ac3885f7294717ead69bd34765d89bb5c746360
Kartike Tiwari <84660976+23kartik@users.noreply.github.com>
2022-04-20 14:00:57 +0530

    Time: 5 ms (80.42%), Space: 7.5 MB (95.42%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-         vector<bool>dp(s.size(),false);
+         vector<bool>dp(s.size()+1,false);
         dp[0]=true;
        for(int i = 0; i <= s.size(); i++)
         {
@@ -16,6 +16,7 @@ public:
                     }
                 }
             }
-        }return dp[s.size()];
+        }
+        return dp[s.size()];
     }
 };
