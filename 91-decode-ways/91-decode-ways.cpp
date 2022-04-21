2ecf1eed9d41fb01a60308babf21d5715a139dea
Kartike Tiwari <84660976+23kartik@users.noreply.github.com>
2022-04-21 16:19:06 +0530

    Time: 0 ms (100.00%), Space: 6 MB (92.60%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n=s.size();
-        vector<int>dp(n+1,0);
-        dp[n]=1;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]!='0')
-                dp[i]+=dp[i+1];
-            if(i+1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6'))
-                dp[i]+=dp[i+2];
+        if(s[0]=='0') return 0;
+        if(s.size()==1) return 1;
+        int dp[n];
+        dp[0]=1;
+       dp[1] = (s[0] == '1' || s[0] == '2' && s[1] < '7' ? 1 : 0) + (s[1] != '0');
+        for(int i=2;i<n;i++){
+           if (s[i] == '0' && (s[i - 1] > '2' || s[i - 1] == '0')) return 0;
+            dp[i] = s[i] != '0' ? dp[i - 1] : 0;
+           if (s[i - 1] == '1' || s[i - 1] == '2' && s[i] < '7') dp[i] += dp[i - 2];
         }
-        return dp[0];
+        return dp[n-1];
     }
 };
