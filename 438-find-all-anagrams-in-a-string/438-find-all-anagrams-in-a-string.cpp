81a7506170128c7262b6f9834c6f613e1c7c7329
Kartike Tiwari <84660976+23kartik@users.noreply.github.com>
2022-05-08 21:54:27 +0530

    Time: 57 ms (19.78%), Space: 15.9 MB (7.57%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        map<char,int>pmp;
+        vector<int>res;
+        for(int i=0;i<p.size();i++)
+            pmp[p[i]]++;
+        map<char,int>smp;
+        int i,j,flag=0;
+        i=0,j=0;
+       while(i<=j&&j<s.size()){
+           if(j==p.size()){
+               flag=1;
+           }
+           if(flag==0){
+               smp[s[j]]++;
+               j++;
+           }
+           else{
+               if(smp[s[i]] == 1) smp.erase(s[i]);
+               else smp[s[i]]--;
+                smp[s[j]]++;
+               i++,j++;
+           }
+          
+           if(smp==pmp)
+               res.push_back(i);
+       }
+        
+        return res;
+    }
+};
