62bd55e674c404d2f0f9f7729469c6e8465dc9e0
Kartike Tiwari <84660976+23kartik@users.noreply.github.com>
2022-05-10 19:08:17 +0530

    Time: 230 ms (71.32%), Space: 63.8 MB (87.68%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.cpp
+++ 886-possible-bipartition/886-possible-bipartition.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    void dfs(int i,bool &result,vector<int>&color,vector<int>adj[]){
+    bool dfs(int i,vector<int>&color,vector<int>adj[]){
         if(color[i]==0)
             color[i]=1;
         for(auto u:adj[i]){
-            if(color[u]==0){
+            if(color[i]==color[u])
+                return false;
+            if(color[u]==0){     
             color[u]=-color[i];
-            dfs(u,result,color,adj);
+            dfs(u,color,adj);
             }
-            if(color[i]==color[u]){
-                result=false;
-                return;
-            }
-  
+           
+            
         }
-         
+            return true;
     }
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         vector<int>adj[n+1];
         for(int i=0;i<dislikes.size();i++){
             adj[dislikes[i][0]].push_back(dislikes[i][1]);
-             adj[dislikes[i][1]].push_back(dislikes[i][0]);
+            adj[dislikes[i][1]].push_back(dislikes[i][0]);
         }
-        bool result=true;
-        
+      
         vector<int>color(n+1,0);
         for(int i=1;i<=n;i++){
             if(color[i]==0){
-              dfs(i,result,color,adj);      
+                if(!dfs(i,color,adj))
+                    return false;
             }
         }
-        return result;
+        return true;
     }
 };
