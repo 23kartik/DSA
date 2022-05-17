eb1373e42dfbb08692e81e8c84b795b16e4ae8d4
Kartike Tiwari <84660976+23kartik@users.noreply.github.com>
2022-05-18 01:26:25 +0530

    Time: 521 ms (69.79%), Space: 89.7 MB (67.51%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,23 +1,17 @@
+bool cmp(vector<int>& a, vector<int>& b) {return a[1] < b[1];}
 class Solution {
+    
 public:
+    
     int findMinArrowShots(vector<vector<int>>& points) {
-        sort(points.begin(),points.end());
-        int cnt=1,i=0,balloon=points[0][1];
-        
-       while(i<points.size()-1){
-           if(balloon>=points[i+1][0]&&balloon<=points[i+1][1]){
-               i++;
-           }  
-           else if(balloon>points[i+1][0]&&balloon>points[i+1][1]){
-               balloon=points[i+1][1];
-               i++;
-           }
-           else{
-               balloon=points[i+1][1];
-               cnt++;
-               
-           }
-       }
-        return cnt;
+         sort(points.begin(), points.end(), cmp);
+        int ans = 0, arrow = 0;
+        for (int i = 0; i < points.size(); i ++) {
+            if (ans == 0 || points[i][0] > arrow) {
+                ans ++;
+                arrow = points[i][1];
+            }
+        }
+        return ans;
     }
 };
