c383d23108df0c658ba225762ee6d814cc3e47d5
Kartike Tiwari <84660976+23kartik@users.noreply.github.com>
2022-06-19 21:58:34 +0530

    Time: 205 ms (54.06%), Space: 8.4 MB (64.47%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -4,8 +4,7 @@ public:
     int n = str.size();
     bool table[n][n];
  
-    memset(table, 0, sizeof(table));
- 
+    memset(table, 0, sizeof(table)); 
     int maxLength = 1;
  
     for (int i = 0; i < n; i++)
@@ -18,16 +17,12 @@ public:
             start = i;
             maxLength = 2;
         }
-    }
- 
-    
+    }    
     for (int k = 3; k <= n; k++) {
         for (int i = 0; i < n - k + 1; i++) {
             int j = i + k - 1;
- 
             if (table[i + 1][j - 1] && str[i] == str[j]) {
                 table[i][j] = true;
- 
                 if (k > maxLength) {
                     start = i;
                     maxLength = k;
