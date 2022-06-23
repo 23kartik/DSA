3954f644c3f797fe6f86294ca41ba02f525e43eb
Kartike Tiwari <84660976+23kartik@users.noreply.github.com>
2022-06-24 00:33:19 +0530

    Time: 2267 ms (5.01%), Space: 8 MB (36.09%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
-    bool search(int i,int j,vector<vector<char>>& board,string word,int index){
-        if(index==word.size())   return true; 
-        if(i<0||i>board.size()-1||j<0||j>board[0].size()-1)  return false;
-        if(board[i][j]!=word[index]) return false;
-        board[i][j]='0';
-        bool newSearch=search(i+1,j,board,word,index+1)||search(i-1,j,board,word,index+1)||search(i,j+1,board,word,index+1)||search(i,j-1,board,word,index+1);
-        board[i][j]=word[index];
-        return newSearch;
-        
-        
+    bool search(int i,int j,int k,string temp,vector<vector<int>> &visited,vector<vector<char>>& board, string word){
+        if(temp.size()==word.size()){
+            if(temp==word)
+                return true;
+            return false;
+        }
+        if(i>=0 && i<board.size() && j>=0 && j<board[0].size()){
+            if(visited[i][j])
+                return false;
+            if(board[i][j]!=word[k])
+                return false;
+            temp+=board[i][j];
+            visited[i][j]=1;
+            bool a=search(i,j+1,k+1,temp,visited,board,word);     
+            bool b=search(i+1,j,k+1,temp,visited,board,word);
+            bool c=search(i,j-1,k+1,temp,visited,board,word);
+            bool d=search(i-1,j,k+1,temp,visited,board,word);    
+            visited[i][j]=0;
+            if(a||b||c||d)
+                return true;
+        }
+        return false;
     }
+    
     bool exist(vector<vector<char>>& board, string word) {
-      
-            for(int i=0;i<board.size();i++){
-                for(int j=0;j<board[0].size();j++){
-                     if(search(i,j,board,word,0))
-                         return true;
+        int m=board.size(),n=board[0].size();
+        vector<vector<int>> visited(m,vector<int> (n,0));
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]==word[0]){
+                    if(search(i,j,0,"",visited,board,word))
+                        return true;
                 }
             }
-           return false;
+        }
+        return false;
     }
 };
