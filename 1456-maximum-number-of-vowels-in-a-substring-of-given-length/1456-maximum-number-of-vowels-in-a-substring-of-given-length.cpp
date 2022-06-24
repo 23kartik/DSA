5aaeda8915c39d283cee8b5e10c57ef9f846094c
Kartike Tiwari <84660976+23kartik@users.noreply.github.com>
2022-06-24 22:17:36 +0530

    Time: 34 ms (44.65%), Space: 10 MB (62.58%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -3,23 +3,17 @@ public:
     int maxVowels(string s, int k) {
         int cnt=0,maxi=0;
         for(int i=0;i<k;i++){
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
-                cnt++;
-            }
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u') cnt++;
         }
         int i=1,j=k;
         maxi=cnt;
         while(j<s.size()){
-            if(s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'||s[j]=='u'){
-                cnt++;            
-            }
-         if(s[i-1]=='a'||s[i-1]=='e'||s[i-1]=='i'||s[i-1]=='o'||s[i-1]=='u'){
-                    cnt--;
-                   
-                }
+         if(s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'||s[j]=='u') cnt++;
+         if(s[i-1]=='a'||s[i-1]=='e'||s[i-1]=='i'||s[i-1]=='o'||s[i-1]=='u') cnt--;
+            
             maxi=max(maxi,cnt);
             j++;
-                i++;
+            i++;
         }
         return maxi;
     }
