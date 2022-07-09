aaf5da06f0695035716666efa0b4e884f8498530
Kartike Tiwari <84660976+23kartik@users.noreply.github.com>
2022-07-10 00:26:00 +0530

    Time: 2624 ms (5.02%), Space: 7 MB (24.77%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int ans=INT_MAX;
+    void helper(int start,vector<int>&nums,vector<int>&v,int k){
+        if(start==nums.size()){
+            int maxi=INT_MIN;
+            for(int i=0;i<k;i++){
+                maxi=max(maxi,v[i]);
+            }
+            ans=min(maxi,ans);
+            return;
+        }
+        for(int i=0;i<k;i++){
+            v[i]+=nums[start];
+            helper(start+1,nums,v,k);
+            v[i]-=nums[start];
+        }
+    }
+   
+    int distributeCookies(vector<int>& nums, int k) {
+     vector<int>v(k,0);
+        helper(0,nums,v,k);
+        return ans;
+    }
+};
