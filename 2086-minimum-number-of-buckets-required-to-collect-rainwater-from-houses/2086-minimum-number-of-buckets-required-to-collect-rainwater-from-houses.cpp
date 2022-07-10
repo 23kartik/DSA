f7a829f450f69f22b37c1629e9ffeee4c3462aed
Kartike Tiwari <84660976+23kartik@users.noreply.github.com>
2022-07-11 00:47:46 +0530

    Time: 27 ms (63.18%), Space: 10.4 MB (92.80%) - LeetHub

---
+++ 2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses/2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumBuckets(string street) {
+       for(int i=0;i<street.size();i++){
+        if(street[i]=='H'){
+            if(i>0&&street[i-1]=='B') continue;
+            else if(i<street.size()-1&&street[i+1]=='.')
+                street[i+1]='B';
+            else if(i>0&&street[i-1]=='.')
+                street[i-1]='B';
+            else return -1;
+        }   
+       }
+        
+        return count(street.begin(),street.end(),'B');
+        
+    }
+};
